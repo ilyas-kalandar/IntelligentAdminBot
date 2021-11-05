82426e6c74066148c964d6bcdc87ebfe76803110
ilyas <ilyas_kalandar@outlook.com>
2021-11-05 12:57:30 +0500

    Refactoring for core.py and userbot.py

--- filters.py
+++ filters.py
@@ -44,21 +44,3 @@ class ReadOnlyFilter(BoundFilter):
 
     async def check(self, message: types.Message):
         return self.read_only == built_vars.READ_ONLY[message.chat.id]
-
-
-class IsServedChat(BoundFilter):
-    """
-    Filter that checks message.chat.id for existing in settings.py
-    """
-
-    key = 'is_served_chat'
-
-    def __init__(self, is_served_chat: bool):
-        """
-        Ctor
-        :param is_served_chat: Check for chat serving
-        """
-        self.is_served_chat = is_served_chat
-
-    async def check(self, message: types.Message) -> bool:
-        return message.chat.id in settings.SERVED_CHATS == self.is_served_chat
