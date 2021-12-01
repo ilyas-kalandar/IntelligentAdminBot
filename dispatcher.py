ff5f00061c3ac729f3431fd2efccb84a5c360d3f
Ilyas Kalandar <ilyas_kalandar@outlook.com>
2021-12-01 16:39:13 +0500

    Some changes for using new configurator

--- dispatcher.py
+++ dispatcher.py
@@ -1,16 +1,16 @@
 from aiogram import Bot, Dispatcher
 
-import settings
 from filters import IsAdminFilter, CanRestrictMembers, ReadOnlyFilter
 from userbot import UserBot
+from built_vars import CONFIG
 
 # bot
 
-bot = Bot(settings.BOT_TOKEN, parse_mode='html')
+bot = Bot(CONFIG.get_param("bot", "Token"), parse_mode='html')
 dp = Dispatcher(bot)
 
 # user bot
-user_bot = UserBot(settings.API_HASH, settings.API_ID)
+user_bot = UserBot(CONFIG.get_param("Pyrogram", "ApiHash"), CONFIG.get_param("Pyrogram", "ApiID"))
 
 # register filters
 dp.filters_factory.bind(IsAdminFilter)
